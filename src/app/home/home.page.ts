afcb1bea89794a9a45523a833904f13a32600854
Sujuforny <pengny2002@gmail.com>
2023-08-31 17:17:22 +0700

    app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  BASEURL : String = "http://localhost:8080/api/v1/"
+  
+  constructor(private http: HttpClient) {}
+  body : any = {
+    email :  "",
+    password : "",
+    comformPassword : ""
+  }
 
-  constructor() {}
+  onEmailChange(event: any) {
+    this.body.email = event.detail.value;
+  }
+  onPasswordChange(event: any) {
+    this.body.password = event.detail.value;
+  }
+  onComformPasswordChange(event: any) {
+    this.body.password = event.detail.value;
+    console.log(this.body)
 
+  }
+  public submit(){
+    this.http.post(this.BASEURL+"auth/register",this.body).subscribe((data)=>{
+      console.log(data)
+    })
+  }
 }
